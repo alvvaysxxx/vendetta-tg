0c55d64511592dbd2081e68331d215e6b576f898
alvvaysxxx <alvvaysxxx@gmail.com>
2024-02-05 20:45:21 +0300

    htthgthjtn

--- bot.js
+++ bot.js
@@ -5,6 +5,6 @@ const API_KEY_BOT = "6855579648:AAF29wJqMxl_QCdy9RCjesGojgSduJxJrLY";
 const url = "https://vendetta-tg.vercel.app";
 
 const bot = new TelegramBot(API_KEY_BOT);
-bot.setWebHook(`${url}/bot${API_KEY_BOT}`);
+await bot.setWebHook(`${url}/bot${API_KEY_BOT}`);
 
 module.exports = bot;
