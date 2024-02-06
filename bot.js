a899ff26f725dbb74e1c8ca855b838815d2bf542
alvvaysxxx <alvvaysxxx@gmail.com>
2024-02-06 22:27:52 +0300

    пепап

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const API_KEY_BOT = "6855579648:AAF29wJqMxl_QCdy9RCjesGojgSduJxJrLY";
 
-const url = "https://vendetta-tg.vercel.app";
+const url = "https://c2kq4hl1-8000.euw.devtunnels.ms";
 
 const bot = new TelegramBot(API_KEY_BOT);
 
