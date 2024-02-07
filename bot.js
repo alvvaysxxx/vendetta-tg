1c0519e797a930301305c626e7899b8f7c4c0626
alvvaysxxx <alvvaysxxx@gmail.com>
2024-02-07 22:35:43 +0300

    polling thing

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const API_KEY_BOT = "6855579648:AAF29wJqMxl_QCdy9RCjesGojgSduJxJrLY";
 
 const url = "http://62.113.102.166:8000";
 
-const bot = new TelegramBot(API_KEY_BOT);
-
-bot.setWebHook(`${url}/bot${API_KEY_BOT}`);
+const bot = new TelegramBot(API_KEY_BOT, {
+  polling: true,
+});
 
 module.exports = bot;
