ca4f834fc3d768088e969b8a3f0df415b001621a
Urion <114020016+alvvaysxxx@users.noreply.github.com>
2024-02-08 22:29:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,Authorization"
+    "*"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
