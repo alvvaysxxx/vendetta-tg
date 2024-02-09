fa689da535410c7fda4afed01ee1767cb2aab29d
Urion <114020016+alvvaysxxx@users.noreply.github.com>
2024-02-09 17:00:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET", "POST");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "*"
