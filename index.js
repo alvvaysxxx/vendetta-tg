ecb1a8db547de1bb77428895408c34e2067fbf9e
alvvaysxxx <alvvaysxxx@gmail.com>
2024-02-09 17:19:56 +0300

    tuyj

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "*"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
